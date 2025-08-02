35cf4fb8337741c2d6944a4d105ad607f927a61b
paticow <patimatanazareth@gmail.com>
2025-08-02 18:29:13 -0400

    Clear button works

--- calc.js
+++ calc.js
@@ -33,21 +33,6 @@ function operate(num1, operator, num2) {
     }
 }
 
-function operate(num1, operator, num2) {
-    switch (operator) {
-        case "+":
-            return add(num1, num2);
-        case "-":
-            return subtract(num1, num2);
-        case "*":
-            return multiply(num1, num2);
-        case "/":
-            return divide(num1, num2);
-        default:
-            return null;
-    }
-}
-
 
 document.addEventListener('DOMContentLoaded', function() {
     const screen = document.getElementById("screen");
@@ -75,22 +60,25 @@ numbers.forEach(button => {
 symbol.forEach(button => {
     button.addEventListener("click", function() {
         operator = button.textContent;
+        screen.textContent = operator;
         enteringNum2 = true; 
         console.log(`operator = ${operator}`);
     });
 });
 
-const equal = document.getElementById("equal");
 equal.addEventListener("click", function() {
-    operate(Number(num1), operator, Number(num2));
+    let result = operate(Number(num1), operator, Number(num2));
+    screen.textContent = result; 
 });
 
-    const clearButton = document.getElementById("clear");
+
+const clearButton = document.getElementById("clear");
     clearButton.addEventListener("click", function() {
         screen.textContent = ""; 
         operator = null; 
         num1 = ""; 
         num2 = ""; 
+        enteringNum2 = false;
     });
 });
 
